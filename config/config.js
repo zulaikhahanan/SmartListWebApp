4110772a52ea8fa3c64e79f0e784fc6e613cd0c5
zulaikhahanan <aikhx00@gmail.com>
2023-02-15 21:00:54 +0800

    first commit

--- config/config.js
+++ config/config.js
@@ -4,4 +4,5 @@ module.exports = {
     envPort: process.env.PORT,
     dbURL: 'mongodb+srv://aikhx:iGP6XbABTTOoKrB0@project.grgeq0f.mongodb.net/smartList?retryWrites=true&w=majority',
     sessionKey: process.env.SESSION_SECRET,
+  
 };
